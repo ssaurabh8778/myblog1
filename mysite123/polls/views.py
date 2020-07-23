79ee6fa830abd0e441828f82e921a1f11fe31349
ssaurabh8778 <67585859+ssaurabh8778@users.noreply.github.com>
2020-07-23 10:33:36 +0530

    Add files via upload

---
+++ mysite123/polls/views.py
@@ -0,0 +1,69 @@
+<<<<<<< HEAD
+from django.shortcuts import render
+import sqlite3
+# Create your views here.
+from django.shortcuts import render
+import pyodbc
+
+def index(request):
+    return render(request,'polls/welcome.html')
+
+conn = sqlite3.connect('/home/ssaurabh8778/ssaurabh8778.pythonanywhere.com/mysite123/polls/data.sqlite')
+cursor = conn.cursor()
+cursor.execute('')
+
+s1 = cursor.fetchall()
+
+
+
+
+def home(request):
+    return render(request,'polls/home.html', {'s1' : s1})
+
+def total_filled1(request):
+    return render(request, 'polls/total_filled.html', {'s1' : s1})
+
+def data_interface1(request):
+    return render(request, 'polls/data_interface.html', {'s1' : s1})
+
+def testing1(request):
+    return render(request, 'polls/testing.html', {'s1' : s1})
+
+def testing_data1(request):
+    return render(request, 'polls/testing_data.html', {'s1' : s1})
+=======
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+import pyodbc
+
+def index(request):
+    return render(request,'polls/welcome.html')
+
+conn = pyodbc.connect(
+    r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+    r'DBQ=polls/DATABASE.mdb;')
+cursor = conn.cursor()
+cursor.execute('select * from DATA1')
+
+s1 = cursor.fetchall()
+
+
+
+
+def home(request):
+    return render(request,'polls/home.html', {'s1' : s1})
+
+def total_filled1(request):
+    return render(request, 'polls/total_filled.html', {'s1' : s1})
+
+def data_interface1(request):
+    return render(request, 'polls/data_interface.html', {'s1' : s1})
+
+def testing1(request):
+    return render(request, 'polls/testing.html', {'s1' : s1})
+
+def testing_data1(request):
+    return render(request, 'polls/testing_data.html', {'s1' : s1})
+>>>>>>> 97c628cb69f003a77bcc5e5d7b9cf7ad30d53317
