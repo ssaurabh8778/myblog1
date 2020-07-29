a34178c8bf73cd1cb9773cad3c985c7027f54501
ssaurabh8778 <ssaurabh8778@gmail.com>
2020-07-29 20:49:02 +0530

    29.07.20 15

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite123_local.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite123.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
