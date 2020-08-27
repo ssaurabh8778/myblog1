f0817e64efec0aee07739755c1c366f254ca6dd5
ssaurabh8778 <ssaurabh8778@gmail.com>
2020-08-27 20:19:36 +0530

    n

--- mysite123/settings.py
+++ mysite123/settings.py
@@ -137,8 +137,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
 )
 MEDIA_URL = '/media/'
+STATIC_ROOT = '/static/'
+
 '''STATIC_ROOT = '/home/ssaurabh8778/ssaurabh8778.pythonanywhere.com/polls/static'''
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-]
+]'''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
