5480b909cb3d32288adc5ccf4e20896f2625071c
ssaurabh8778 <ssaurabh8778@gmail.com>
2021-03-09 08:15:00 +0530

    new commit

--- mysite123/urls.py
+++ mysite123/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('rezume/', include('rezume.urls')),
+    path('tkphapi/', include('tkphapi.urls')),
 ]
 
